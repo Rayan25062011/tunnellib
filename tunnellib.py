21c855a5bf6a6556299b48848d4bccf1af7511d4
Rayan25062011 <101386337+Rayan25062011@users.noreply.github.com>
2022-10-27 19:12:05 +0300

    Add files via upload

---
+++ tunnellib.py
@@ -0,0 +1,30 @@
+import urllib
+import requests
+import os
+import sys
+import time
+import exceptions
+import webbrowser
+
+class tunnellib(object):
+    def __init__(self, web):
+        self.web = web
+
+    def secure_connection(self):
+        while True:
+            if self.web.startswith('http://'):
+                url = self.web.replace('http://', 'https://', 1)
+                r = urllib.request.urlopen(self.web)
+                webbrowser.open(self.web)
+            else:
+                raise exceptions.WebConfError("Connection is already secured.")
+
+
+    def unsecure_connection(self):
+        if self.web.startswith('http://'):
+            url = self.web.replace('http://', 'https://', 1)
+            r = urllib.request.urlopen(self.web)
+            webbrowser.open(self.web)
+        else:
+            raise exceptions.WebConfError("Connection is already secured.")
+
